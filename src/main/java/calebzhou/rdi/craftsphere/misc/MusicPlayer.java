eaecaf35c034af58983cfe42be0697b792b9d3b5
calebz <portableryanc69@outlook.com>
2022-08-08 23:13:17 +0800

    1.19

--- src/main/java/calebzhou/rdi/craftsphere/misc/MusicPlayer.java
+++ src/main/java/calebzhou/rdi/craftsphere/misc/MusicPlayer.java
@@ -2,39 +2,105 @@ package calebzhou.rdi.craftsphere.misc;
 
 import calebzhou.rdi.craftsphere.util.FileUtils;
 import calebzhou.rdi.craftsphere.util.ThreadPool;
+import net.sourceforge.jaad.aac.Decoder;
+import net.sourceforge.jaad.aac.SampleBuffer;
+import net.sourceforge.jaad.mp4.MP4Container;
+import net.sourceforge.jaad.mp4.api.AudioTrack;
+import net.sourceforge.jaad.mp4.api.Frame;
+import net.sourceforge.jaad.mp4.api.Movie;
+import net.sourceforge.jaad.mp4.api.Track;
 import org.apache.commons.lang3.RandomUtils;
 
 import javax.sound.sampled.*;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class MusicPlayer {
     public static void playStartupMusic(){
         ThreadPool.newThread(()->{
             System.out.println("放音乐");
-            String yyyyMMdd = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+            /*String yyyyMMdd = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
             File musicPlayHistoryFile = new File("logs/musicplay_"+yyyyMMdd);
             if(musicPlayHistoryFile.exists()){
                 System.out.println("今天放过了");
                 return;
-            }
-            try {
-                musicPlayHistoryFile.createNewFile();
-                int musicAmount=11;
-                Clip clip = AudioSystem.getClip();
-                InputStream inputStream = FileUtils.getJarResourceAsStream("music/startup/" + RandomUtils.nextInt(1, musicAmount + 1) + ".wav");
-                BufferedInputStream bufferStream = new BufferedInputStream(inputStream);
-                AudioInputStream stream = AudioSystem.getAudioInputStream(bufferStream);
+            }*/
+            //musicPlayHistoryFile.createNewFile();
+            int musicAmount=16;
+            //Clip clip = AudioSystem.getClip();
+            InputStream inputStream = FileUtils.getJarResourceAsStream("music/startup/" + RandomUtils.nextInt(1, musicAmount + 1) + ".wav");
+            BufferedInputStream bufferStream = new BufferedInputStream(inputStream);
+            play(bufferStream);
+                /*AudioInputStream stream = AudioSystem.getAudioInputStream(bufferStream);
                 clip.open(stream);
-                clip.start();
-            } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
-                throw new RuntimeException(e);
-            }
+                clip.start();*/
 
         });
+    }
+    public static void play(InputStream musicFile)
+    {
+        // local vars
+        byte[]          b;              // array for the actual audio Data during the playback
+        AudioTrack      track;          // track we are playing atm
+        AudioFormat     af;             // the track's format
+        SourceDataLine  line;           // the line we'll use the get our audio to the speaker's
+        Decoder dec;            // decoder to get the audio bytes
+        Frame frame;          //
+        SampleBuffer    buf;            //
+        int             currentTrack;   // index of current track from playlist
+        MP4Container cont;           // container to open the current track with
+        Movie movie;          // and get the content from the container
+        boolean paused=false;
+        try
+        {
+                cont    = new MP4Container((musicFile)); // open titel with random access
+                movie   = cont.getMovie();                          // get content from container,
+                List<Track> content = movie.getTracks();
+                if (content.isEmpty())     {
+                    System.out.println("music content=empty");
+                }
+                track   = (AudioTrack) movie.getTracks().get(0);    // grab first track and set the audioformat
+                af      = new AudioFormat(track.getSampleRate(), track.getSampleSize(), track.getChannelCount(), true, true);
+                line    = AudioSystem.getSourceDataLine(af);        // get a DataLine from the AudioSystem
+                line.open();                                        // open and
+                line.start();                                       // start it
+
+                dec     = new Decoder(track.getDecoderSpecificInfo());
+
+                buf = new SampleBuffer();
+                while(track.hasMoreFrames())                // while we have frames left
+                {
+                    frame = track.readNextFrame();          // read next frame,
+                    dec.decodeFrame(frame.getData(), buf);  // decode it and put into the buffer
+                    b = buf.getData();                      // write the frame data from the buffer to our byte-array
+                    line.write(b, 0, b.length);             // and from there write the byte array into our open AudioSystem DataLine
+
+                    while (paused)                  // check if we should pause
+                    {
+                        Thread.sleep(500);          // if yes, stay half a second
+
+                        if (Thread.interrupted())   // check if we should stop possibly
+                        {
+                            line.close();           // if yes, close line and
+                            return;                 // exit thread
+                        }
+                    }
+
+                    if (Thread.interrupted())       // if not in pause, still check on each frame if we should
+                    {                               // stop. If so
+                        line.close();               // close line and
+                        return;                     // exit thread
+                    }
+                }
+
+                line.close();           // after titel is over, close line
+
+        }
+        catch (LineUnavailableException | IOException | InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/calebzhou/rdi/craftsphere/mixin/mFinishLoad.java
+++ src/main/java/calebzhou/rdi/craftsphere/mixin/mFinishLoad.java
@@ -3,18 +3,25 @@ package calebzhou.rdi.craftsphere.mixin;
 import calebzhou.rdi.craftsphere.misc.LoadProgressDisplay;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementList;
+import net.minecraft.client.gui.screens.packs.PackSelectionModel;
+import net.minecraft.client.renderer.texture.AtlasSet;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.util.profiling.ProfilerFiller;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
-@Mixin(AdvancementList.class)
+@Mixin(ModelBakery.class)
 public class mFinishLoad {
-    @Inject(method = "add",at=@At("TAIL"))
-    private void finishLoad(Map<ResourceLocation, Advancement.Builder> map, CallbackInfo ci){
+    @Inject(method = "uploadTextures",at=@At("TAIL"))
+    private void finishLoad(TextureManager textureManager, ProfilerFiller profilerFiller, CallbackInfoReturnable<AtlasSet> cir){
         LoadProgressDisplay.onFinish();
     }
 }
